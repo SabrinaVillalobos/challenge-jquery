8c1735733aa658f12c4ee3dac6448bfd9aa8b2ea
SabrinaVillalobos <wafflish@gmail.com>
2018-01-08 19:45:59 -0300

    Etapa 3: variable recipesArray

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -17,7 +17,14 @@ function printNews () {
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
+  var highlightedRecipes = [];
+  for (var i = 0; i < recipesArray.length; i++) {
+    if (recipesArray[i].highlighted == true) {
+      highlightedRecipes.push(recipesArray[i]);
+    }
+  }
+  renderRecipe(highlightedRecipes);
+  console.log('Recipes: ', highlightedRecipes);
 }
 
 /*
